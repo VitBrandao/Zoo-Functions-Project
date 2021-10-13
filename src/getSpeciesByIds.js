8acc7ceb28e3363f8600c1fa89e9ca56c3bbc5b7
vitor <vitorsbsilva@gmail.com>
2021-10-13 14:11:26 -0300

    Iniciando projeto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+
+
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  const array = data.species.filter((especie) => {
+    especie.id === ids;
+  });
+
+
+  // for (let index = 0; index < array.species.length; index += 1) {
+  //   if (array.species[index].id === ids) {
+  //     return array.species[index];
+  //   }
+  // }
 }
 
 module.exports = getSpeciesByIds;
+
+console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+
+// console.log(data.species);

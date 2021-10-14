f1f214b26046653168aac8bc33ac0fd07671eb66
vitor <vitorsbsilva@gmail.com>
2021-10-14 16:18:11 -0300

    Requisito 3 feito. Boragit add . git add .

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const dataEmployees = data.employees;
+
+  const employeesFirstName = dataEmployees.find((value) => value.firstName === employeeName);
+  if (employeesFirstName !== undefined) {
+    return employeesFirstName;
+  }
+  const employeesLastName = dataEmployees.find((value) => value.lastName === employeeName);
+  if (employeesLastName !== undefined) {
+    return employeesLastName;
+  }
+
+  const emptyObject = {};
+  return emptyObject;
 }
 
 module.exports = getEmployeeByName;
+
+// console.log(getEmployeeByName('Nelson'));
+// // 'Nigel' 'Nelson'
+// // 'Burl' 'Bethea'

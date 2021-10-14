e9b13a5040baf8bf5751064fae44f932ab84aa60
vitor <vitorsbsilva@gmail.com>
2021-10-14 19:01:29 -0300

    Requisito 5 feito depois de MUITO esforço

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,45 @@
 const data = require('../data/zoo_data');
+// const getAnimalsOlderThan = require('./getAnimalsOlderThan');
 
-function countAnimals(animal) {
-  // seu código aqui
+function checkObject(animal) {
+  const dataSpecies = data.species;
+
+  const checkAnimal = dataSpecies.find((value) => value.name === animal.specie);
+  if (Object.keys(animal).length === 1) {
+    const totalAnimals = checkAnimal.residents.length;
+    return totalAnimals;
+  }
+
+  const animalSex = checkAnimal.residents;
+  const objectSex = animal.sex;
+  let howManyAnimals = 0;
+  for (let index = 0; index < animalSex.length; index += 1) {
+    if (animalSex[index].sex === objectSex) {
+      howManyAnimals += 1;
+    }
+  }
+
+  return howManyAnimals;
 }
 
+function countAnimals(animal) {
+  const dataSpecies = data.species;
+
+  if (animal === undefined) {
+    const totalNumbers = {};
+    for (let index = 0; index < dataSpecies.length; index += 1) {
+      const key = dataSpecies[index].name;
+      const value = dataSpecies[index].residents.length;
+      totalNumbers[key] = value;
+    }
+    return totalNumbers;
+  }
+
+  return checkObject(animal);
+}
 module.exports = countAnimals;
+
+const testObject = { specie: 'bears', sex: 'male' };
+console.log(countAnimals(testObject));
+
+// Com o argumento { specie: 'giraffes', sex: 'female' }, retorna um número, a quantidade de girafas do sexo feminino.
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,51 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+// function isManager(id) {
+//   const dataEmployees = data.employees;
+//   const receivedId = id;
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+//   // for (let index = 0; index < dataEmployees.length; index++) {
+//   //   console.log(dataEmployees[index].managers);
 
-module.exports = { isManager, getRelatedEmployees };
+//   //   if (dataEmployees[index].managers === receivedId) {
+//   //     return true;
+//   //   }
+
+//   // }
+
+//      return false;
+//   // console.log(dataEmployees[0].managers);
+//   // const filterManagers = dataEmployees.filter((value) => value.managers === id);
+//   // console.log(filterManagers);
+
+//   // const verifyManager = dataEmployees.find((value) => value.managers === id);
+
+//   // return verifyManager;
+// }
+
+// function getRelatedEmployees(managerId) {
+//   const checkManager = isManager(managerId);
+
+//   const dataEmployees = data.employees;
+//   const verifyManager = dataEmployees.find((value) => value.id === managerId);
+
+//   if (checkManager === true) {
+//     return verifyManager.responsibleFor;
+//   } else {
+//     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+//   }
+// }
+
+// module.exports = { isManager, getRelatedEmployees };
+
+// // console.log(isManager('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
+// // console.log(getRelatedEmployees('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
+
+// // console.log(data.employees);
+// console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+
+// // 'fdb2543b-5662-46a7-badc-93d960fdc0a8'
+// // '56d43ba3-a5a7-40f6-8dd7-cbb05082383f'
+// // '4b40a139-d4dc-4f09-822d-ec25e819a5ad' espera false
+
+console.log(data.species[2].name);

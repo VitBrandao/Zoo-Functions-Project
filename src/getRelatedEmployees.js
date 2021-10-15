2527af630a2d1e22ba7125686015ffb955f6c61f
vitor <vitorsbsilva@gmail.com>
2021-10-15 13:59:21 -0300

    AGORA SIM requisito 4 feito!

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -5,13 +5,32 @@ function isManager(id) {
   return dataEmployees.some((employee) => employee.managers.includes(id));
 }
 
+function createFinalArray(id) {
+  const dataEmployees = data.employees;
+  const findManager = dataEmployees.find((employee) => employee.id === id);
+  const managerIdentification = findManager.id;
+
+  const finalArray = [];
+  dataEmployees.forEach((value) => {
+    value.managers.forEach((value2) => {
+      if (value2.includes(managerIdentification)) {
+        const name = value.firstName;
+        const last = value.lastName;
+        const fullName = `${name} ${last}`;
+
+        finalArray.push(fullName);
+      }
+    });
+  });
+
+  return finalArray;
+}
+
 function getRelatedEmployees(managerId) {
   const verifyManager = isManager(managerId);
-  const dataEmployees = data.employees;
 
   if (verifyManager === true) {
-    const findManager = dataEmployees.find((employee) => employee.id === managerId);
-    return findManager.responsibleFor;
+    return createFinalArray(managerId);
   }
 
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');

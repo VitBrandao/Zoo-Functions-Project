684a6ba46a3ce500c82f52c95bc4503760a90ac0
vitor <vitorsbsilva@gmail.com>
2021-10-15 15:10:57 -0300

    Mesmo com o lint pedindo uma refatoraçao desnecessaria, requisito 6 esta feito e aprovado por npm

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,45 @@
 const data = require('../data/zoo_data');
 
+// const object = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
+
 function countEntrants(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  const finalObject = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((person) => {
+    if (person.age < 18) {
+      finalObject.child += 1;
+    } else if (person.age >= 18 && person.age < 50) {
+      finalObject.adult += 1;
+    } else {
+      finalObject.senior += 1;
+    }
+  });
+
+  return finalObject;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  const counting = countEntrants(entrants);
+  const childPrice = (counting.child * 20.99);
+  const adultPrice = (counting.adult * 49.99);
+  const seniorPrice = (counting.senior * 24.99);
+
+  const totalPrice = childPrice + adultPrice + seniorPrice;
+  return totalPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };

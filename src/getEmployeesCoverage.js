06e8035cd92703bfe579c30d6a4a642c2777ac64
vitor <vitorsbsilva@gmail.com>
2021-12-03 16:43:22 -0300

    Req.10 - FEITO DE VEZ

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,31 +1,13 @@
 const data = require('../data/zoo_data');
 
-// Função para quando HÁ parâmetros
-function letsFindTheEmployee(object) {
-  const dataEmployees = data.employees;
+const dataEmployees = data.employees;
+const dataSpecies = data.species;
 
-  // Encontrando funcionário correspondente pelo nome, sobrenome ou id
-  const findEmployee = dataEmployees.find((employee) =>
-    employee.firstName === object.name
-    || employee.lastName === object.name
-    || employee.id === object.id);
-
-  // Não encontrou ninguém? Lança o erro
-  if (findEmployee === undefined) {
-    throw new Error('Informações inválidas');
-  }
-
-  // Definindo propriedades que já estão no objeto employees
+// Função auxiliar para letsFindTheEmployee por questões de lint
+function employeeInformation(findEmployee, employeeSpecies, employeeLocations) {
   const employeeId = findEmployee.id;
   const employeeFullName = `${findEmployee.firstName} ${findEmployee.lastName}`;
-
-  // Buscando propriedades no objeto species
-  const dataSpecies = data.species;
   const employeeResponsibleFor = findEmployee.responsibleFor;
-
-  // Encontrando species e locations
-  const employeeSpecies = [];
-  const employeeLocations = [];
 
   for (let index = 0; index < employeeResponsibleFor.length; index += 1) {
     const findSpecie = dataSpecies.find((specie) => specie.id === employeeResponsibleFor[index]);
@@ -33,21 +15,57 @@ function letsFindTheEmployee(object) {
     employeeLocations.push(findSpecie.location);
   }
 
-  // Criando objeto final - já com duas propriedades iniciais
   const finalObject = {
     id: employeeId,
     fullName: employeeFullName,
     species: employeeSpecies,
     locations: employeeLocations,
   };
+  return finalObject;
+}
+
+// Função para quando HÁ parâmetros
+function letsFindTheEmployee(object) {
+  const findEmployee = dataEmployees.find((employee) =>
+    employee.firstName === object.name
+    || employee.lastName === object.name
+    || employee.id === object.id);
+
+  if (findEmployee === undefined) {
+    throw new Error('Informações inválidas');
+  }
+
+  const employeeSpecies = [];
+  const employeeLocations = [];
+
+  const finalObject = employeeInformation(findEmployee, employeeSpecies, employeeLocations);
 
   return finalObject;
 }
 
-const dataEmployees = data.employees;
-const dataSpecies = data.species;
-
 const finalArray = [];
+
+// Função auxiliar para noParameters() por questões de lint
+function mountFullList(employeeSpecies, employeeLocations, index) {
+  const employeeId = dataEmployees[index].id;
+  const employeeFullName = `${dataEmployees[index].firstName} ${dataEmployees[index].lastName}`;
+
+  const employeeResponsibleFor = dataEmployees[index].responsibleFor;
+
+  employeeResponsibleFor.forEach((responsible) => {
+    const findSpecie = dataSpecies.find((specie) => specie.id === responsible);
+    employeeSpecies.push(findSpecie.name);
+    employeeLocations.push(findSpecie.location);
+  });
+
+  const finalObject = {
+    id: employeeId,
+    fullName: employeeFullName,
+    species: employeeSpecies,
+    locations: employeeLocations,
+  };
+  return finalObject;
+}
 
 // Função para quando NÃO HÁ parâmetros
 function noParameters(object) {
@@ -61,28 +79,6 @@ function noParameters(object) {
   }
 
   return finalArray;
-}
-
-function mountFullList(species, locations, index) {
-  const employeeId = dataEmployees[index].id;
-  const employeeFullName = `${dataEmployees[index].firstName} ${dataEmployees[index].lastName}`;
-
-  const employeeResponsibleFor = dataEmployees[index].responsibleFor;
-
-  employeeResponsibleFor.forEach((responsible) => {
-    const findSpecie = dataSpecies.find((specie) => specie.id === responsible);
-    species.push(findSpecie.name);
-    locations.push(findSpecie.location);
-  });
-
-  const finalObject = {
-    id: employeeId,
-    fullName: employeeFullName,
-    species: species,
-    locations: locations,
-  };
-
-  return finalObject;
 }
 
 // FUNÇÃO PRINCIPAL
